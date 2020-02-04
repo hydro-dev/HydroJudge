eec0a1ab986a249ebc51e398ade30f04dead7cf8
masnn <masnn0@outlook.com>
2020-02-04 20:46:04 +0800

    fix: Java VM creation failure

--- jd5/config.js
+++ jd5/config.js
@@ -8,7 +8,7 @@ let config = {
     CACHE_DIR: path.resolve(os.homedir(), '.cache', 'jd5'),
     SYSTEM_MEMORY_LIMIT_MB: 1024,
     SYSTEM_TIME_LIMIT_MS: 16000,
-    SYSTEM_PROCESS_LIMIT: 8,
+    SYSTEM_PROCESS_LIMIT: 32,
     RETRY_DELAY_SEC: 15,
     SANDBOX_ROOT: '/tmp/jd5',
     SANDBOX_POOL_COUNT: 2,
