e26cf42c0cc2dd4a753522769991757eff440446
masnn <masnn0@outlook.com>
2020-02-12 14:09:35 +0800

    No more wget

--- jd5/hosts/vj4.js
+++ jd5/hosts/vj4.js
@@ -19,42 +19,9 @@ module.exports = class AxiosInstance {
         await this.setCookie(this.config.cookie || '');
         await this.ensureLogin();
     }
-    async setCookie(cookie) {
-        this.config.cookie = cookie;
-        this.axios = axios.create({
-            baseURL: this.config.server_url,
-            timeout: 3000,
-            headers: {
-                'accept': 'application/json',
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'cookie': this.config.cookie
-            },
-            transformRequest: [
-                function (data) {
-                    let ret = '';
-                    for (let it in data)
-                        ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&';
-                    return ret;
-                }
-            ]
-        });
-    }
-    async login() {
-        let res = await this.axios.post('login', {
-            uname: this.config.uname, password: this.config.password, rememberme: 'on'
-        });
-        await this.setCookie(res.headers['set-cookie'][0].split(';')[0]);
-    }
-    async ensureLogin() {
-        log.log(`[${this.config.host}] Updating session`);
-        try {
-            await this.axios.get('judge/noop');
-        } catch (e) {
-            await this.login();
-        }
-    }
     async problem_data_version(domain_id, pid, retry = 3) {
         let location, err;
+        await this.ensureLogin();
         try {
             await this.axios.get(`d/${domain_id}/p/${pid}/data`, { maxRedirects: 0 });
         } catch (res) {
@@ -65,10 +32,7 @@ module.exports = class AxiosInstance {
             } else if (res.response.status == 404)
                 throw new FormatError(`Testdata not found: ${domain_id}/${pid}`);
             else {
-                if (retry) {
-                    await this.ensureLogin();
-                    return await this.problem_data_version(domain_id, pid, retry - 1);
-                }
+                if (retry) return await this.problem_data_version(domain_id, pid, retry - 1);
                 res.config = res.request = null;
                 err = res;
                 console.log(err);
@@ -94,13 +58,15 @@ module.exports = class AxiosInstance {
     }
     async problem_data(domain_id, pid, save_path, retry = 3) {
         log.info(`Getting problem data: ${this.config.host}/${domain_id}/${pid}`);
+        await this.ensureLogin();
         let tmp_file_path = path.resolve(CACHE_DIR, `download_${this.config.host}_${domain_id}_${pid}`);
         try {
+            let res = await this.axios.get(`${this.config.server_url}d/${domain_id}/p/${pid}/data`, { responseType: 'stream' });
+            let w = await fs.createWriteStream(tmp_file_path);
+            res.data.pipe(w);
             await new Promise((resolve, reject) => {
-                child.exec(`wget "${this.config.server_url}d/${domain_id}/p/${pid}/data" -O ${tmp_file_path} --header=cookie:${this.config.cookie}`, e => {
-                    if (e) reject(e);
-                    else resolve();
-                });
+                w.on('finish', resolve);
+                w.on('error', reject);
             });
             await mkdirp(path.dirname(save_path));
             await new Promise((resolve, reject) => {
@@ -119,37 +85,16 @@ module.exports = class AxiosInstance {
         }
         return save_path;
     }
-    async process_data(folder) {
-        let files = await fsp.readdir(folder), ini = false;
-        for (let i of files)
-            if (i.toLowerCase() == 'config.ini') {
-                ini = true;
-                await fsp.rename(`${folder}/${i}`, folder + '/config.ini');
-                break;
-            }
-        if (ini) {
-            for (let i of files)
-                if (i.toLowerCase() == 'input')
-                    await fsp.rename(`${folder}/${i}`, folder + '/input');
-                else if (i.toLowerCase() == 'output')
-                    await fsp.rename(`${folder}/${i}`, folder + '/output');
-            files = await fsp.readdir(folder + '/input');
-            for (let i of files)
-                await fsp.rename(`${folder}/input/${i}`, `${folder}/input/${i.toLowerCase()}`);
-            files = await fsp.readdir(folder + '/output');
-            for (let i of files)
-                await fsp.rename(`${folder}/output/${i}`, `${folder}/output/${i.toLowerCase()}`);
-        }
-    }
     async record_pretest_data(rid, save_path) {
         log.info(`Getting pretest data: ${this.config.host}/${rid}`);
         let tmp_file_path = path.resolve(CACHE_DIR, `download_${this.config.host}_${rid}`);
         await this.ensureLogin();
+        let res = await this.axios.get(`records/${rid}/data`, { responseType: 'stream' });
+        let w = await fs.createWriteStream(tmp_file_path);
+        res.data.pipe(w);
         await new Promise((resolve, reject) => {
-            child.exec(`wget "${this.config.server_url}records/${rid}/data" -O ${tmp_file_path} --header=cookie:${this.config.cookie}`, e => {
-                if (e) reject(e);
-                else resolve();
-            });
+            w.on('finish', resolve);
+            w.on('error', reject);
         });
         await mkdirp(path.dirname(save_path));
         await new Promise((resolve, reject) => {
@@ -187,6 +132,62 @@ module.exports = class AxiosInstance {
             this.ws.once('open', () => { resolve(); });
         });
         log.info(`[${this.config.host}] Connected`);
+    }
+    async setCookie(cookie) {
+        this.config.cookie = cookie;
+        this.axios = axios.create({
+            baseURL: this.config.server_url,
+            timeout: 3000,
+            headers: {
+                'accept': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'cookie': this.config.cookie
+            },
+            transformRequest: [
+                function (data) {
+                    let ret = '';
+                    for (let it in data)
+                        ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&';
+                    return ret;
+                }
+            ]
+        });
+    }
+    async login() {
+        let res = await this.axios.post('login', {
+            uname: this.config.uname, password: this.config.password, rememberme: 'on'
+        });
+        await this.setCookie(res.headers['set-cookie'][0].split(';')[0]);
+    }
+    async ensureLogin() {
+        log.log(`[${this.config.host}] Updating session`);
+        try {
+            await this.axios.get('judge/noop');
+        } catch (e) {
+            await this.login();
+        }
+    }
+    async process_data(folder) {
+        let files = await fsp.readdir(folder), ini = false;
+        for (let i of files)
+            if (i.toLowerCase() == 'config.ini') {
+                ini = true;
+                await fsp.rename(`${folder}/${i}`, folder + '/config.ini');
+                break;
+            }
+        if (ini) {
+            for (let i of files)
+                if (i.toLowerCase() == 'input')
+                    await fsp.rename(`${folder}/${i}`, folder + '/input');
+                else if (i.toLowerCase() == 'output')
+                    await fsp.rename(`${folder}/${i}`, folder + '/output');
+            files = await fsp.readdir(folder + '/input');
+            for (let i of files)
+                await fsp.rename(`${folder}/input/${i}`, `${folder}/input/${i.toLowerCase()}`);
+            files = await fsp.readdir(folder + '/output');
+            for (let i of files)
+                await fsp.rename(`${folder}/output/${i}`, `${folder}/output/${i.toLowerCase()}`);
+        }
     }
     async retry(queue) {
         this.consume(queue).catch(() => {
