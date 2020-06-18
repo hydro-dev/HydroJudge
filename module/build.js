dfdf1f54e7c1bdb37c93ca7f618b4c33a555c978
undefined <masnn0@outlook.com>
2020-06-18 12:55:19 +0800

    1.3.0

--- module/build.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const yaml = require('js-yaml');
-
-exports.prebuild = async function prebuild() {
-    let lang = fs.readFileSync(path.resolve(__dirname, '../examples/langs.yaml')).toString();
-    lang = yaml.safeLoad(lang);
-    fs.writeFileSync(path.resolve(__dirname, '__langs.json'), JSON.stringify(lang));
-};
--- module/setting.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    langs: {
-        type: 'text',
-        default: JSON.stringify(require('./__langs.json')),
-        description: 'Language file settings',
-    },
-};
